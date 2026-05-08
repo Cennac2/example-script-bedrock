79a979af7c659e3672d2aa02210ba2bef63c5da1
Cennac <139062520+Cennac2@users.noreply.github.com>
2026-05-08 20:26:43 +0100

    Fix comments and improve runInterval usage

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 //Import world and system class
 import { world, system } from "@minecraft/server";
 
-//runInterval re-runs code inside callback till you stop it
+//system.runInterval runs the code inside the callback repeatedly.
 system.runInterval(() => {
-    world.sendMessage("Hello, World!"); //Send Hello world! in game chat
-}, 20); //the 20 tells it to run every 1 second (20 ticks = 1 sec)
+    world.sendMessage("Hello, World!"); // Send the message "Hello World"
+}, 20); // The second argument (20) is a number telling the function how often to run the callback in ticks
 
 
